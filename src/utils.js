b4cedcf9b3a36595f76bf13362509401560faf23
“João <eng.joao.pedro@outlook.com>
2023-01-31 19:04:43 -0300

    refactor: create array of array

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,15 @@
-const paginate = () => {}
+const paginate = (followers) => {
+  const itemPerPage = 10;
+  const pages = Math.ceil(followers.length / itemPerPage);
 
-export default paginate
+  //array of array
+  const newFollowers = Array.from({ length: pages }, (_, index) => {
+    //index of each array
+    const start = index * itemPerPage;
+    //start and end of new arrays
+    return followers.slice(start, start + itemPerPage);
+  });
+  return newFollowers;
+};
+
+export default paginate;
